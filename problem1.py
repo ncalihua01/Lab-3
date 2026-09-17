4cfac373023545147f91f5f143f06cb0f9cdb3fc
ncalihua01 <ncalihua01@saintmarys.edu>
2026-09-16 21:01:55 -0400

    problem1.py

--- problem1.py
+++ problem1.py
@@ -1,8 +1,26 @@
-def flatten(list):
+def flatten(nested_list):
   """
-  ### add to this, including adding additional doctests
+  nested list -> flattened list
+  flatten takes in a list with internal nesting and returns a single list
 
   >>> flatten([1,[[2],3],4,[5,6]])
   [1,2,3,4,5,6]
+  
+  >>> flatten([1,[4,5],6,[7,[9]]])
+  [1,4,5,6,7,9]
+  
+  >>> flatten([[4,5],6,[9]])
+  [4,5,6,9]
   """
+  result = []
+  for x in nested_list:
+    if isinstance(x, list):
+      result.extend(flatten(x))
+    else:
+      result.append(x)
+  return result
+
+
+
+
   
