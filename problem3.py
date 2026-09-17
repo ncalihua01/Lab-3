69562f294e81a55e50ae8d8b5461e23e47e74ed3
ncalihua01 <ncalihua01@saintmarys.edu>
2026-09-16 21:47:53 -0400

    problem3.py

--- problem3.py
+++ problem3.py
@@ -1,2 +1,13 @@
 def count_vowels(string):
-  
+  """
+  string -> number of vowels
+  count_vowels takes in a character string and returns the number of vowels in given character string
+
+  >>> count_vowels("September")
+  3
+  >>> count_vowels("Purple")
+  2
+  >>> count_vowels("Myth")
+  0
+  """
+  return sum(vowel in "a e i o u" for vowel in string.lower())
