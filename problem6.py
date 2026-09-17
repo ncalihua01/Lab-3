2d33a7bfc9c291e80317bcb1260853c6fece3f6f
ncalihua01 <ncalihua01@saintmarys.edu>
2026-09-17 00:00:04 -0400

    problem6.py

--- problem6.py
+++ problem6.py
@@ -1,9 +1,26 @@
 def merge_sets(set1, set2):
-
+  """
+  set1 and set2 -> merged set
+  """
+  result = set()
+  for x in set1:
+    for y in set2:
+      result.add(x + y)
+  return result
+  
 def word_scramble_sets(set):
   """
-  ### add stuff here
+  world_scramble_sets returns a set containing all possible word scrambles
 
   >>> word_scramble_sets({"a", "b", "c", "c"})
   {"abc", "acb", "bac", "bca", "cab", "cba"}
   """
+  if len(set) == 1:
+    return set
+  result = set()
+
+  for letter in set:
+    remaining = set - {letter}
+    for word in word_scramble_sets(remaining):
+      result.add(letter + word)
+  return result
